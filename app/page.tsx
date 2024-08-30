7156c8fe19232f4a1e7c72001a14bfbd4590bec3
Hiroki Chiyoda <contact@arafipro.com>
2024-08-30 13:00:25 +0900

    feat: トップページを変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { validateRequest } from "@/auth";
 import Link from "next/link";
 
-export default function Home() {
+export const runtime = "edge";
+export default async function Home() {
+  const { user } = await validateRequest();
   return (
-    <main className="flex flex-col gap-4">
+    <main className="flex flex-col items-center gap-4">
+      <h1>{user ? user.username : "No Login User"}</h1>
       <Link href="/sign-up">Sign Up</Link>
       <Link href="/sign-in">Login</Link>
       <Link href="/sign-out">Sign Out</Link>
